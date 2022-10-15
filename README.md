0545083a3a0f836bc4247b8c169e1597992f37dd
hung117 <snaketrex0001@gmail.com>
2022-10-15 14:49:48 +0700

    Animation
    2D animation via
    - redraw window
    - using double buffer, swap them to achieve smooth animation

--- README.md
+++ README.md
@@ -4,3 +4,47 @@ use "ASDW" to move the graphs around;
 Z to zoom in
 X to zoom out
 Press "ESC" on your keyboard to Exit the program
+
+# Question:
+
+## 1.0 For Loop
+
+Ban đầu em để for/while loop trong hàm Display và Animation_Naive (thử nhiều lần, lần thì loop trong Display, lần trong Animation_Naive. Chứ không phải trong cả hai cùng lúc ạ)
+Thì em nhận thấy rằng chương trình unresponsive và lúc sau đơ. Em đoán là do bản thân Glut cũng có loop rồi nên gọi loop trong loop như thế thì không ổn ạ.
+Nên em sử dụng "glutPostRedisplay" để glut biết cần phải redraw window.
+
+$Em không biết rằng có cách nào khác để gọi loop không ạ, vì trong sách người ta dùng được for ntn ạ:
+
+```
+float cx = 0.3, cy = 0.2; //center of the window
+float H, W = 1.2, aspect = 0.7; // window properties
+set the viewport
+for(int frame = 0; frame < NumFrames; frame++) // for each frame
+{
+clear the screen // erase the previous figure
+W *= 0.7; // reduce the window width
+H = W * aspect; // maintain the same aspect ratio
+setWindow(cx - W, cx + W, cy - H, cy + H); //set the next window
+hexSwirl(); // draw the object
+}
+
+```
+
+## 2.0 Tweening speed
+
+Với cách vẽ hình tween ở Animation_Naive, quan sát bằng mắt, em thấy
+
+-   tốc độ của animation không đồng nhất, như kiểu nó phụ thuộc vào clockspeed hay ntn đấy em không rõ lắm ạ.
+-   đôi khi animation chạy rất nhanh, đôi khi lại chậm
+
+trong sách người ta nói rằng
+
+```
+a). Instantaneous erasure of the current figure;
+b). A (possibly) slow redraw of the new figure.
+```
+
+thì em đoán là nói về việc animation bị flickering thôi ạ.
+Mong thầy giải thích hộ em nó phụ thuộc vào cái gì và để animation có tốc độ đều nhau thì làm như thế nào ạ. Và có cách nào để check sự nhất quán đấy ngoài cách nhìn bằng mắt không ạ ?
+
+# Em cảm ơn ạ
--- Src/main.cpp
+++ Src/main.cpp
@@ -41,33 +41,9 @@ double cur_Vp_R = 1024;
 double cur_Vp_B = 0;
 double cur_Vp_T = 780;
 /* -- LOCAL VARIABLES ---------------------------------------------------- */
-
-/* ----------------------------------------------------------------------- */
-/* Function    : void drawDot( GLint x, GLint y )
- *
- * Description : Draw a point at location (x, y) in the window.
- *
- * Parameters  : GLint x : X coordinate of point to draw.
- *               GLint y : X coordinate of point to draw.
- *
- * Returns     : void
- */
-
 void drawDot(GLint x, GLint y)
 {
 }
-
-/* ----------------------------------------------------------------------- */
-/* Function    : void myInit( void )
- *
- * Description : Initialize OpenGL and the window where things will be
- *               drawn.
- *
- * Parameters  : void
- *
- * Returns     : void
- */
-
 void myInit(void)
 {
     glClearColor(0.0f, 0.4f, 0.4f, 1.0f);
@@ -80,16 +56,33 @@ void myInit(void)
     glViewport(0, 640, 0, 480);
 }
 
-/* ----------------------------------------------------------------------- */
-/* Function    : void myDisplay( void )
- *
- * Description : This function gets called everytime the window needs to
- *               be redrawn.
- *
- * Parameters  : void
- *
- * Returns     : void
- */
+/* ------------------------------PARAMETERS----------------------------------------- */
+GLintPoint A[] = {
+    GLintPoint(600, 300),
+    GLintPoint(500.0, 335.0),
+    GLintPoint(310.0, 400.0),
+    GLintPoint(310.0, 400.0),
+    GLintPoint(185.0, 350.0),
+    GLintPoint(40.0, 300.0),
+    GLintPoint(40.0, 40.0),
+    GLintPoint(600.0, 40.0),
+    GLintPoint(600.0, 40.0)};
+GLintPoint B[] = {
+    GLintPoint(600.0, 300.0),
+    GLintPoint(450.0, 300.0),
+    GLintPoint(400.0, 350.0),
+    GLintPoint(200.0, 350.0),
+    GLintPoint(150.0, 300.0),
+    GLintPoint(40.0, 300.0),
+    GLintPoint(40.0, 200.0),
+    GLintPoint(600.0, 200.0),
+};
+float t = 0.0f; // t stands for time. I supposed
+float deltaT = 0.001f;
+
+bool bRunning = true;
+int i_NumFrame = 200;
+int i_frame = 0;
 //--------------- setWindow ---------------------
 void setWindow(float left, float right, float bottom, float top)
 {
@@ -103,16 +96,61 @@ void setViewport(float left, float right, float bottom, float top)
     glViewport(left, bottom, right - left, top - bottom);
 }
 //---------------- Display utility functions  ------------------
+void drawHouse()
+{
 
+    glBegin(GL_LINE_LOOP);
+    for (auto point : A)
+    {
+        glVertex2f(point.x, point.y);
+    }
+    glEnd();
+    glFlush();
+}
+void drawCar()
+{
+    glBegin(GL_LINE_LOOP);
+    for (auto point : B)
+    {
+        glVertex2f(point.x, point.y);
+    }
+    glEnd();
+    glFlush();
+}
+float tweening(float a, float b, float t)
+{
+    return (1 - t) * a + t * b;
+}
+void Animation_Naive() // without double Buffer
+{
+    glBegin(GL_LINE_LOOP);
+    if (t == 0.0f || t + deltaT < 0.0f || t == 1.0f || t + deltaT > 1.0f)
+        if (t == 0.0f || t + deltaT < 0.0f)
+        {
+            deltaT = 0.005;
+        }
+        else if (t == 1.0f || t + deltaT > 1.0f)
+        {
+            deltaT = -0.005;
+        }
+
+    cout << "t: " << t << ", deltaT: " << deltaT << endl;
+    for (int i = 0; i < 8; i++)
+    {
+        // cout << "t: " << t << ", deltaT: " << deltaT << endl;
+        float pX = tweening(A[i].x, B[i].x, t);
+        float pY = tweening(A[i].y, B[i].y, t);
+        glVertex2f(pX, pY);
+    }
+    glEnd();
+    glFlush();
+}
+void Animation_Buffer()
+{
+}
 void drawFuncGraph()
 {
     setWindow(-50.0, 250.0, -50.0, 50.0);
-
-    // glBegin(GL_LINE_STRIP);
-    // glColor3f(0.6, 0.6, 0.4);
-    // for (GLfloat x = -4.0; x < 4.0; x += 0.1) // draw the plot
-    //     glVertex2f(x, sin(3.14159 * x) / (3.14159 * x));
-    // glEnd();
     int multiplier = 6;
     setViewport(cur_Vp_L + 150, cur_Vp_R + 150, cur_Vp_B, cur_Vp_T);
     glBegin(GL_LINE_STRIP);
@@ -162,12 +200,36 @@ void drawPolyLine(const char *filePath)
     glFlush();
     instream.close();
 }
+
 void myDisplay()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    const char *filePath = "../Res/dinosaur.dat";
-    drawPolyLine(filePath);
-    drawFuncGraph();
+
+    // for (int frame = 0; frame < i_NumFrame; ++frame)
+    // {
+    //     t += deltaT;
+    //     glClear(GL_COLOR_BUFFER_BIT); // clear Screen to draw new shape, hence creating illusion of animation.
+    //     // drawHouse();
+    //     // drawCar();
+    //     Animation_Naive();
+    // }
+    // while (bRunning)
+    // {
+    //     if (i_frame < i_NumFrame)
+    //     {
+    //         t += deltaT;
+    //         glClear(GL_COLOR_BUFFER_BIT); // clear Screen to draw new shape, hence creating illusion of animation.
+    //         // drawHouse();
+    //         // drawCar();
+    //         Animation_Naive();
+    //         i_frame++;
+    //     }
+    // }
+    glClear(GL_COLOR_BUFFER_BIT); // clear Screen to draw new shape, hence creating illusion of animation.
+    // drawHouse();drawCar();
+    Animation_Naive();
+    glutSwapBuffers();
+    t += deltaT;
+    glutPostRedisplay(); // tell glut that it's window's need to be redrawn
 }
 void myKeyboard(unsigned char theKey, int x, int y)
 {
@@ -175,6 +237,7 @@ void myKeyboard(unsigned char theKey, int x, int y)
     {
     case 27:
         cout << "Exit program" << endl;
+        bRunning = false;
         exit(-1); // terminate the program
         break;
     case 'w':
@@ -227,15 +290,17 @@ int main(int argc, char *argv[])
     // Initialize GLUT.
     glutInit(&argc, argv);
     // Set the mode to draw in.
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    // glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     // Set the window size in screen pixels.
     glutInitWindowSize(640, 480);
     // Set the window position in screen pixels.
     glutInitWindowPosition(100, 60);
     // Create the window.
     glutCreateWindow("Lab");
-    // Set the callback funcion to call when we need to draw something.
+    // reDraw window
     glutDisplayFunc(myDisplay);
+
     // Listen for keyboard events
     glutKeyboardFunc(myKeyboard);
     // Initialize some things.
